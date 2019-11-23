738050907b95d720400a8bd802c0bce71b4047b8
Craig Wylie <wyliec94@gmail.com>
2019-11-23 15:17:08 +1100

    Fixed linting errors

--- server.js
+++ server.js
@@ -1,23 +1,21 @@
-var express = require('express');
-var path = require('path');
-var serveStatic = require('serve-static');
-var axios = require('axios');
+var express = require("express");
+var serveStatic = require("serve-static");
+var axios = require("axios");
 
-app = express();
+var app = express();
 app.use(express.json());
 
-app.post('/emails', (req, res) => {
-  axios.post('https://tt-email-gateway.nnnco.io/v1/api/emails', req.body)
-  .then(response => {
-    res.json(response.data);
-  })
-  .catch(error => {
-    console.log("Error");
-    res.sendStatus(error.status);
-  });
+app.post("/emails", (req, res) => {
+  axios
+    .post("https://tt-email-gateway.nnnco.io/v1/api/emails", req.body)
+    .then(response => {
+      res.json(response.data);
+    })
+    .catch(error => {
+      res.sendStatus(error.status);
+    });
 });
 
 app.use(serveStatic(__dirname + "/dist"));
 var port = process.env.PORT || 5000;
 app.listen(port);
-console.log('server started '+ port);
